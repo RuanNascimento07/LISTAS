93b42e4b123e12768cc80d59ca555665c007164a
RuanNascimento07 <162449821+RuanNascimento07@users.noreply.github.com>
2025-05-19 08:43:11 -0300

    Add files via upload

---
+++ funcoesArray.js
@@ -0,0 +1,17 @@
+const strings = ["Banana", "Maçã", "Tomate", "Goiaba"]
+const numeros = [20,40, 80, 10]
+
+strings.push("Abacate")
+console.log(strings)
+
+strings.unshift("Jaca")
+console.log(strings)
+
+strings.splice(3, 1)
+console.log(strings)
+strings.splice(3, 0, "Pêra")
+console.log(strings)
+
+strings.pop()
+console.log(strings)
+
